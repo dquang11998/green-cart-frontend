e25219c09ff8b4fbab9151e8c77c668f4fbe8f7d
dquang11998 <dquang11998@gmail.com>
2026-01-13 16:50:08 +0700

    feat: bottombanner, footer, login, category, all products, product details, sreach

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import { Navbar } from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/login";
+import AllProducts from "./pages/AllProducts";
+import Categories from "./components/Categories";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
 
       <Toaster />
 
@@ -19,8 +28,12 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProductDetails />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
